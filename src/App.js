b28457b4d66f97cd9c642d30c4f19c8784f8bf69
akash <akashes5753279@gmail.com>
2024-03-19 13:06:57 +0530

    created-routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+       
+   
     </div>
   );
 }
